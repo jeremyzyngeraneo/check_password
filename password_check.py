439ba5a2f284d30bb645e577a737288b2b588b93
jeremyzyngeraneo <jzynger@aneo.fr>
2023-08-09 13:52:38 +0200

    add password_checker

---
+++ password_check.py
@@ -0,0 +1,39 @@
+# install module : pip install requests
+# import
+import requests
+
+# your password
+user_password = input("Please enter the password to check : \n")
+
+# Check if password is in the 500 worst passwords
+
+
+def worst_500_passwords(password):
+    # Download the Seclist
+    print('Beginning check in 500 worst passwords')
+    url = 'https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/500-worst-passwords.txt'
+    retrieve = requests.get(url)
+
+    content = retrieve.content
+
+    if str(password) in str(content):
+        print("Your password is in the 500 worst passwords! change it !!!\n")
+        exit(0)
+
+# Check if password is in the 10k worst passwords
+
+
+def worst_10k_passwords(password):
+    print('Beginning check in 10k worst passwords')
+    url = 'https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10k-most-common.txt'
+    retrieve = requests.get(url)
+
+    content = retrieve.content
+
+    if str(password) in str(content):
+        print("Your password is in the 10k worst passwords! change it !!!\n")
+        exit(0)
+
+
+worst_500_passwords(user_password)
+worst_10k_passwords(user_password)
