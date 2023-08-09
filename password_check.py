39285087c6618c5ff14720c16fd84bf63c0370e2
jeremyzyngeraneo <jzynger@aneo.fr>
2023-08-09 16:01:55 +0200

    fix lint

--- password_check.py
+++ password_check.py
@@ -1,8 +1,10 @@
-
+"""Check password"""
 # install module : pip install requests
-# import
-import requests
 
+# imports
+import sys
+
+import requests
 # your password
 user_password = input("Please enter the password to check : \n")
 
@@ -10,32 +12,36 @@ user_password = input("Please enter the password to check : \n")
 
 
 def worst_500_passwords(password):
+    """ check if my password is in the 500 worst passwords
+    """
     # Download the Seclist
     print('Beginning check in 500 worst passwords')
-    url = 'https://raw.githubusercontent.com/danielmiessler/SecLists/'
-    'master/Passwords/Common-Credentials/500-worst-passwords.txt'
+    url = 'https://raw.githubusercontent.com/danielmiessler/SecLists/'\
+        'master/Passwords/Common-Credentials/500-worst-passwords.txt'
     retrieve = requests.get(url, timeout=5)
 
     content = retrieve.content
 
     if str(password) in str(content):
         print("Your password is in the 500 worst passwords! change it !!!\n")
-        exit(0)
+        sys.exit(0)
 
 # Check if password is in the 10k worst passwords
 
 
 def worst_10k_passwords(password):
+    """ check if my password is in the 10k worst passwords
+    """
     print('Beginning check in 10k worst passwords')
-    url = 'https://raw.githubusercontent.com/danielmiessler/SecLists/'
-    'master/Passwords/Common-Credentials/10k-most-common.txt'
+    url = 'https://raw.githubusercontent.com/danielmiessler/SecLists/'\
+        'master/Passwords/Common-Credentials/10k-most-common.txt'
     retrieve = requests.get(url, timeout=5)
 
     content = retrieve.content
 
     if str(password) in str(content):
         print("Your password is in the 10k worst passwords! change it !!!\n")
-        exit(0)
+        sys.exit(0)
 
 
 worst_500_passwords(user_password)
